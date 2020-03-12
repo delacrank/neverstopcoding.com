d6a028645786ce429ec51b97317b8e427c68c8e3
juan arias <jcarias86@gmail.com>
2020-03-12 02:25:56 -0400

    angular stuff

--- Includes/Left_Angular_6.php
+++ Includes/Left_Angular_6.php
@@ -7,5 +7,6 @@
 <li><a href="../Angular_6/Angular3.html">Binding &amp; Filtering</a></li>
 <li><a href="../Angular_6/Angular4.html">Methods</a></li>
 <li><a href="../Angular_6/Angular5.html">Component &amp; Template</a></li>
+<li><a href="../Angular_6/Angular6.html">Sports App</a></li>
     </ul></li></ul>
 </div>
