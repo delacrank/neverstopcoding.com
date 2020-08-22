0b4b29641dd3bd4148296e3f786f572a8fed3231
Juan Arias <jcarias86@gmail.com>
2020-08-22 02:25:09 -0400

    added some bash and algorithm stuff

--- Includes/Left_Bash.php
+++ Includes/Left_Bash.php
@@ -4,5 +4,7 @@
     <li><a href="Bash.html">Intro to Unix</a></li>
     <li><a href="Bash1.html">Directory Commands</a></li>
     <li><a href="Bash2.html">Directory Cmds Cont.</a></li>
+    <li><a href="Bash3.html">Bash Scripts</a></li>
+    <li><a href="Bash4.html">Bash Scripts cont.</a></li>
     </ul></li></ul>
 </div>
