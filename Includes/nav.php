b695fdfd4e307536bb0c66873ce173086eeb6af3
Juan Arias <jcarias86@gmail.com>
2024-06-27 11:47:10 -0400

    fixed navigation bar links

--- Includes/nav.php
+++ Includes/nav.php
@@ -4,32 +4,32 @@
 <li><a href="../index.php">Home</a></li>
 <li onmouseover='' ><a>Web Dev</a>
     <ul>
-        <li><a href="Html/Html.php">Html</a></li>
-        <li><a href="CSS/Css.php">CSS</a></li>
-        <li><a href="Js/Js.php">Javascript</a></li>
-        <li><a href="Jscript/Jscript.php">Js EMCA 6</a></li>
-        <li><a href="JQuery/JQuery.php">JQuery</a></li>
-        <li><a href="Angular_6/Angular.php">Angular_6</a>
-        <li><a href="Typescript/Typescript.php">Typescript</a>
-        <li><a href="Ajax/Ajax.php">Ajax</a></li>
-        <li><a href="PhP/PhP.php">PhP</a></li>
-        <li><a href="CodeIgniter/ci.php">Code Igniter</a></li>
-	<li><a href="Java_Web/Java_Web.php">Java_Web</a></li>
-        <li><a href="Web%20Applications/Web_Apps.php">Web Applications</a></li>
-        <li><a href="MySQL/MySQL.php">MySQL</a></li>
-        <li><a href="SEO/Seo.php">SEO</a></li>
+        <li><a href="../Html/Html.php">Html</a></li>
+        <li><a href="../CSS/Css.php">CSS</a></li>
+        <li><a href="../Js/Js.php">Javascript</a></li>
+        <li><a href="../Jscript/Jscript.php">Js EMCA 6</a></li>
+        <li><a href="../JQuery/JQuery.php">JQuery</a></li>
+        <li><a href="../Angular_6/Angular.php">Angular_6</a>
+        <li><a href="../Typescript/Typescript.php">Typescript</a>
+        <li><a href="../Ajax/Ajax.php">Ajax</a></li>
+        <li><a href="../PhP/PhP.php">PhP</a></li>
+        <li><a href="../CodeIgniter/ci.php">Code Igniter</a></li>
+	    <li><a href="../Java_Web/Java_Web.php">Java_Web</a></li>
+        <li><a href="../Web%20Applications/Web_Apps.php">Web Applications</a></li>
+        <li><a href="../MySQL/MySQL.php">MySQL</a></li>
+        <li><a href="../SEO/Seo.php">SEO</a></li>
     </ul>    
 </li>
 <li onmouseover='' ><a>Programming</a>
     <ul>
-        <li><a href="C/C.php">C</a></li>
-        <li><a href="C++/C++.php">C++03</a></li>
-        <li><a href="Cpp/Cpp.php">C++11</a></li>
-        <li><a href="Cpp_Std_Lib/Cpp_Std_Lib.php">C++_Std_Lib</a></li>
-        <li><a href="Assembly/Assembly.php">Assembly</a></li>
-        <li><a href="Java/Java.php">Java</a></li>
-        <li><a href="Java_GUI/Java_GUI.php">Java_GUI</a></li>
-        <li><a href="Algorithms/Algorithms.php">Algorithms</a></li>
+        <li><a href="../C/C.php">C</a></li>
+        <li><a href="../C++/C++.php">C++03</a></li>
+        <li><a href="../Cpp/Cpp.php">C++11</a></li>
+        <li><a href="../Cpp_Std_Lib/Cpp_Std_Lib.php">C++_Std_Lib</a></li>
+        <li><a href="../Assembly/Assembly.php">Assembly</a></li>
+        <li><a href="../Java/Java.php">Java</a></li>
+        <li><a href="../Java_GUI/Java_GUI.php">Java_GUI</a></li>
+        <li><a href="../Algorithms/Algorithms.php">Algorithms</a></li>
     </ul>
 </li>
 <li><a href="../login/Login.php">Login</a></li>
